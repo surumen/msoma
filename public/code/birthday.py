885b7f877c497660b1dd074080ae3375cac8e4d9
Moses Surumen <moses.susrumen@gmail.com>
2017-05-08 17:30:51 +0300

    functions

---
+++ public/code/birthday.py
@@ -0,0 +1,17 @@
+
+def happyBirthdayMoses():
+	print("Happy Birthday to you!")
+	print("Happy Birthday to you!")
+	print("Happy Birthday, dear Moses!")
+	print("Happy Birthday to you!")
+
+
+#### ADD THE LINES BELOW TO YOUR FILE  ###
+
+# We need to define a main function that will execute our first function
+def main():
+	happyBirthdayMoses()
+
+# this will tell your terminal to run the main function
+if __name__ == '__main__':
+	main()
