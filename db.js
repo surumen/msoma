7d55c7a9fd9d4da90aa5d908a713e397183c754b
Moses Surumen <moses.susrumen@gmail.com>
2017-06-01 15:36:17 +0300

    database

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 module.exports = {
-	 'url' : 'mongodb://localhost/test'
+	 // 'url' : 'mongodb://localhost/test'
+	 'url' : 'mongodb://heroku_d6jqbpw0:v3u3upr1nahjstman4fq3n5ueu@ds161041.mlab.com:61041/heroku_d6jqbpw0'
 }
