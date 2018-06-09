f255c32b579d7ba6ec53068d6f01cc38e0a3f6a4
Moses Surumen <moses@Mosess-MacBook-Pro.local>
2018-06-08 23:14:16 -0700

    all

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log('Express server listening on port', port)
 })
