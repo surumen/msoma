faf78299eda0049bc19c0a5e36ee3b567e2ddc70
Victor Korir <vkkorir@gmail.com>
2018-06-09 21:20:01 -0700

    major server login code

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const cookieSession = require('cookie-session')
-const passport = require('passport')
-const bodyParser = require('body-parser')
-const keys = require('./server/config/keys')
-const path = require('path');
-require('./server/models/User')
-require('./server/services/googleAuth')
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const keys = require("./server/config/keys");
+const path = require("path");
+require("./server/models/User");
+require("./server/services/googleAuth");
 
 const app = express();
-mongoose.connect(keys.mongoURI)   // Connect mongodb
+const port = process.env.PORT || 5000;
+mongoose.connect(keys.mongoURI); // Connect mongodb
+
+require("./server/routes/routes")(app);
 
 // Use middlewares {cookie session, passport}
 app.use(
   cookieSession({
-    maxAge: '30d',
+    maxAge: "30d",
     keys: [keys.cookieKey]
   })
-)
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(bodyParser.json())
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/dist"));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
-// Start the app by listening on the default Heroku port
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log('Express server listening on port', port)
-})
+app.listen(port, () => console.log(`Server running on port ${port}`));
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,2 +1,26 @@
-const passport = require('passport')
+const passport = require("passport");
 
+module.exports = app => {
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
+    })
+  );
+
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", (req, res) => {
+      res.redirect("/"); // Redirect user after logging in
+    })
+  );
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.redirect("/"); // Redirect user to home after logging out
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
+};
