fffa225338829bad0ca2dd8960c4294c12d7965c
Victor Korir <vkkorir@gmail.com>
2018-06-09 18:17:38 -0700

    node js server set-up

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-//Install express server
 const express = require('express');
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const keys = require('./server/config/keys')
 const path = require('path');
+require('./server/models/User')
+require('./server/services/googleAuth')
 
 const app = express();
+mongoose.connect(keys.mongoURI)   // Connect mongodb
+
+// Use middlewares {cookie session, passport}
+app.use(
+  cookieSession({
+    maxAge: '30d',
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(bodyParser.json())
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
@@ -13,7 +32,7 @@ res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log('Express server listening on port', port)
 })
---
+++ server/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String,
+  facebookId: String
+});
+
+mongoose.model("users", userSchema);
