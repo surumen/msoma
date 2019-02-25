15f516f2a3df7772c6e7215eb92646abc6953681
Moses Surumen <moses.susrumen@gmail.com>
2019-02-24 16:50:49 -0800

    pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Training from './pages/Training';
+import Technology from './pages/Technology';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/technology' component={Technology} />
+              <Route path='/training' component={Training} />
+          </Switch>
+    </BrowserRouter>
     );
   }
 }
---
+++ src/pages/Training.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => 
+    <div className="jumbotron">
+        <h1>Training Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+    </div>
